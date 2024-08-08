84a800045b3791773f80874b0b473736c08a884a
Antonio Gómez Rodríguez <antoniogomezrodriguez2000@gmail.com>
2024-08-08 18:04:24 +0200

    Update ctes.php

--- helpers/ctes.php
+++ helpers/ctes.php
@@ -7,12 +7,6 @@
     $password = "4690aGa!=$$";  
     $dbname = "cim";*/
 
-    //Datos del servidor del CIM
-    $servername = "lldn637.servidoresdns.net";
-    $username = "qaik701";
-    $password = "Ant0n10Cabr0n";
-    $dbname = "qaik695";
-
     $options = array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
